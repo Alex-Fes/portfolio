0639bded672bbfcbfe8579a262a44a810d0e8335
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-08-26 17:48:53 +0300

    Updates

---
+++ precache-manifest.279e0ec1150691317c77d85a5793cce4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1b0e2a5d6a478c2290",
+    "url": "/portfolio/static/css/main.bd45d589.chunk.css"
+  },
+  {
+    "revision": "3e1b0e2a5d6a478c2290",
+    "url": "/portfolio/static/js/main.d6dfcf00.chunk.js"
+  },
+  {
+    "revision": "0af1133c6ccd0c49cf2e",
+    "url": "/portfolio/static/js/runtime~main.4c091af5.js"
+  },
+  {
+    "revision": "e586f00946ad494efef7",
+    "url": "/portfolio/static/js/2.cac3a9f0.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/portfolio/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "856d9c472e1122d9f703bd5db1f72922",
+    "url": "/portfolio/index.html"
+  }
+];
