458fa592f37d11857ef6cc34438b666851f168d9
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-10-12 22:45:02 +0300

    add "predeploy" "deploy" github-pages

--- src/mainBlock/Main.tsx
+++ src/mainBlock/Main.tsx
@@ -8,7 +8,7 @@ function Main() {
             <div className={`${styleContainer.container} ${style.mainContainer}`}>
             <div className={style.text}>
               <span>Hi everyone!</span>
-            <h1>I`m Aleksey Fesenko.</h1>
+            <h1>I'm Aleksey Fesenko.</h1>
                 <p>It`s my portfolio.</p>
                 <p>I will be a front-end developer.</p>
             </div>
