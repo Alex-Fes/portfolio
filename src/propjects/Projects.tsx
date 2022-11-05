5613ef9669c022ed6bd2ff0efec1961b7218c79b
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-11-05 15:06:53 +0100

    add scss, refactor

--- src/propjects/Projects.tsx
+++ src/propjects/Projects.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import style from './Projects.module.css'
+import style from './Projects.module.scss'
 import styleContainer from "../common/styles/Container.module.css";
 import Project from "./project/Project";
 import Title from "../common/components/title/Title";
+import todoImage from "./../assets/image/todolist.jpg"
+import socialImage from "./../assets/image/socialNetwork.jpg"
+
+
+export type styleObjType = {
+    backgroundImage: string
+}
 
 
 function Projects() {
+    const socialNetwork: styleObjType = {
+        backgroundImage: `url(${socialImage})`,
+
+    };
+    const todolist: styleObjType = {
+        backgroundImage: `url(${todoImage})`
+    };
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectContainer}`}>
-               <Title text={'Projects'}/>
+                <Title text={'Projects'}/>
                 <div className={style.projects}>
-                    <Project title={'Social Network'}
+                    <Project style={socialNetwork}
+                             title={'Social Network'}
                              description={'text description text description text description text description text description'}/>
-                    <Project title={'Todolist'} description={'Sed ornare tellus a odio bibendum, at tristique sapien malesuada. Proin sagittis maximus accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida quam sit amet elit varius tempor. Pellentesque purus eros, blandit eu mollis vel, commodo eget orci. Proin vel hendrerit ex.'}/>
+                    <Project style={todolist}
+                        title={'Todolist'}
+                        description={'Sed ornare tellus a odio bibendum, at tristique sapien malesuada. Proin sagittis maximus accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida quam sit amet elit varius tempor. Pellentesque purus eros, blandit eu mollis vel, commodo eget orci. Proin vel hendrerit ex.'}/>
 
                 </div>
             </div>
--- src/propjects/project/Project.tsx
+++ src/propjects/project/Project.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import style from './Project.module.css'
+import style from './Project.module.scss'
+import {styleObjType} from "../Projects";
 
 
 type ProjectPropsType = {
     title: string
     description: string
+    style: styleObjType
 }
 
 function Project(props: ProjectPropsType) {
     return (
         <div className={style.project}>
-            <div className={style.picture}>
+            <div style={props.style} className={style.picture}>
                 <a href="" className={style.viewBtn}>Watch</a>
             </div>
             <div className={style.projectInfo}>
-            <h3 className={style.projectTitle}>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+                <h3 className={style.projectTitle}>{props.title}</h3>
+                <span className={style.description}>{props.description}</span>
             </div>
         </div>
     )
