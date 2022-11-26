bffca5f40f6d733e9396f0a22f6c16c954a5838a
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-11-26 18:33:58 +0100

    need to understand 'transition', how it is works? need to understand 'animation', how it is works? add skill infographic

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,26 +3,47 @@ import style from './Skills.module.scss'
 import styleContainer from "../common/styles/Container.module.scss";
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
-import Infographic from "../common/components/Infographic/Infographic";
 
 function Skills() {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-               <Title text={'Skills'}/>
+                <Title text={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill title={'React'} description={'text description' }/>
-                    <Skill title={'JS'} description={'text description text description text description text description text description text description'}/>
+                    <Skill title={'React'} description={'text description'}/>
+                    <Skill title={'JS'}
+                           description={'text description text description text description text description text description text description'}/>
                     <Skill title={'CSS'} description={'text description'}/>
                 </div>
 
-                {/*<div className={style.infographic}>*/}
-                {/*    <h4>Infographic</h4>*/}
-                {/*    <Infographic/>*/}
-                {/*    <Infographic/>*/}
-                {/*    <Infographic/>*/}
 
-                {/*</div>*/}
+                <div className={style.infographic}>
+                    <h4>Infographic</h4>
+                    <div className={style.skillBar}>
+                        skill bar
+
+
+                        <div className={style.progressItem}>
+                            <div className={style.itemIn}>
+                                <h3>React</h3>
+                                <span > "80%"</span>
+                            </div>
+                        </div>
+                        <div className={style.bgWrap}>
+                            <div className={style.progressBg}>
+
+                            </div>
+                        </div>
+
+
+
+
+
+
+                    </div>
+
+
+                </div>
 
             </div>
         </div>
