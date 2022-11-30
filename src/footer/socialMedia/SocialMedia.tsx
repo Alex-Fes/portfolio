692ba08e5e8d395f4995a160b7095c061c47dfed
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-11-30 22:13:09 +0100

    try to add fontawesome

--- src/footer/socialMedia/SocialMedia.tsx
+++ src/footer/socialMedia/SocialMedia.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import style from './SocialMedia.module.scss'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact } from '@fortawesome/free-brands-svg-icons'
 
 
 
 function SocialMedia() {
     return (
         <div className={style.social}>
-            <div className={style.icon}></div>
+            <div className={style.icon}>
+                <FontAwesomeIcon icon="fa-brands fa-facebook" />
+            </div>
         </div>
     )
 }
