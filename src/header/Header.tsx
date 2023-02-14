d1420fcc5a86be013b6cd36d36021a1a926cb84c
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-14 22:13:56 +0100

    refactor: add BurgerNav to Header.tsx

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import BurgerNav from '../navigation/burgerNav/BurgerNav'
 import Navigation from '../navigation/Navigation'
 
 import style from './Header.module.scss'
@@ -7,8 +8,10 @@ import style from './Header.module.scss'
 function Header() {
   return (
     <div id="header" className={style.header}>
-      <h4>Portfolio</h4>
+      {/*<h4>Portfolio</h4>*/}
+
       <Navigation />
+      <BurgerNav />
     </div>
   )
 }
