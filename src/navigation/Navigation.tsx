b59d6cee7fac6a7fbe0f54d3b09d41c5ecba15a9
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-14 22:17:35 +0100

    refactor: add react-scroll

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,14 +1,64 @@
 import React from 'react'
 
+import { Link } from 'react-scroll'
+
 import style from './Navigation.module.scss'
 
 function Navigation() {
   return (
     <div className={style.main}>
-      <a href="#">Home</a>
-      <a href="#aboutMe">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#contacts">Contact</a>
+      <Link
+        activeClass={style.active}
+        to="main"
+        spy={true}
+        smooth={true}
+        hashSpy={true}
+        offset={50}
+        duration={500}
+        delay={100}
+        isDynamic={true}
+      >
+        Home
+      </Link>
+      <Link
+        activeClass={style.active}
+        to="aboutMe"
+        spy={true}
+        smooth={true}
+        hashSpy={true}
+        offset={50}
+        duration={500}
+        delay={100}
+        isDynamic={true}
+      >
+        About
+      </Link>
+      <Link
+        activeClass={style.active}
+        to="projects"
+        spy={true}
+        smooth={true}
+        hashSpy={true}
+        offset={50}
+        duration={500}
+        delay={100}
+        isDynamic={true}
+      >
+        Projects
+      </Link>
+      <Link
+        activeClass={style.active}
+        to="contacts"
+        spy={true}
+        smooth={true}
+        hashSpy={true}
+        offset={-215}
+        duration={500}
+        delay={100}
+        isDynamic={true}
+      >
+        Contact
+      </Link>
     </div>
   )
 }
