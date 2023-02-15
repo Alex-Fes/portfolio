bac1caf10909cb617a9cbafc5ba96b350e42b9ca
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-15 13:46:47 +0100

    refactor: completed burger menu

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -8,8 +8,6 @@ import style from './Header.module.scss'
 function Header() {
   return (
     <div id="header" className={style.header}>
-      {/*<h4>Portfolio</h4>*/}
-
       <Navigation />
       <BurgerNav />
     </div>
