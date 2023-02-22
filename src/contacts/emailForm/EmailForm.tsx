56c591fe22fae10b13bc5f1b0a8837e4a8b8a866
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-22 20:21:26 +0100

    refactor: add TextField, add error handle, add axios request to send message

--- src/contacts/emailForm/EmailForm.tsx
+++ src/contacts/emailForm/EmailForm.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 
+import { TextField, ThemeProvider } from '@mui/material'
 import { useFormik } from 'formik'
 
+import { sendMessage } from '../api/gmailAPI'
+
 import style from './EmailForm.module.scss'
+import { theme } from './theme'
 
 const validate = (values: FeedbackValuesType) => {
   const errors: FormikErrorType = {}
@@ -13,21 +17,13 @@ const validate = (values: FeedbackValuesType) => {
   if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = 'Invalid email address'
   }
-  if (values.message.length < 5) {
-    errors.message = 'Too short message'
-  }
-  // if (errors) {
-  //   setFeedback('error')
-  //   setTimeout(() => {
-  //     setFeedback('idle')
-  //   }, 2000)
-  // }
 
   return errors
 }
 
 export const EmailForm = () => {
   const [feedback, setFeedback] = useState<FeedbackType>('idle')
+  const [isSending, setIsSending] = useState(true)
 
   const formik = useFormik({
     initialValues: {
@@ -37,78 +33,76 @@ export const EmailForm = () => {
     },
     validate,
     onSubmit: (values: FeedbackValuesType) => {
-      console.log(values)
-
-      setFeedback('succeeded')
-      setTimeout(() => {
-        setFeedback('idle')
-      }, 2000)
+      setIsSending(true)
+      sendMessage.send(values).then(res => {
+        setFeedback('succeeded')
+        setIsSending(false)
+        setTimeout(() => {
+          setFeedback('idle')
+        }, 4000)
+      })
     },
   })
 
   return (
-    <form className={style.emailForm} onSubmit={formik.handleSubmit}>
-      <div className={style.itemsWrap}>
-        <div className={style.items}>
-          <div className={style.feedback}>
-            <span className={feedback === 'error' ? style.error : style.errorHide}>
-              Please Fill Required Fields!
-            </span>
-
-            <span className={feedback === 'succeeded' ? style.success : style.successHide}>
-              Your message has been received, we will contact you soon.
-            </span>
-          </div>
-          <div className={style.itemHalf}>
-            <div className={style.inputWrapper}>
-              <input
-                autoComplete="off"
-                id={'name'}
-                type="text"
-                placeholder="Name *"
-                {...formik.getFieldProps('name')}
-              />
-              {/*<TextField*/}
-              {/*  fullWidth*/}
-              {/*  // className={style.input}*/}
-              {/*  id="name"*/}
-              {/*  name="name"*/}
-              {/*  label={formik.errors.name && formik.touched.name ? formik.errors.name : 'Name *'}*/}
-              {/*  variant="outlined"*/}
-              {/*  value={formik.values.name}*/}
-              {/*  onChange={formik.handleChange}*/}
-              {/*  error={formik.touched.name && !!formik.errors.name}*/}
-              {/*  InputProps={{*/}
-              {/*    className: style.input,*/}
-              {/*    style: {*/}
-              {/*      color: 'red',*/}
-              {/*    },*/}
-              {/*  }}*/}
-              {/*  InputLabelProps={{*/}
-              {/*    // style: { color: '#d8ccbe82' },*/}
-              {/*    className: style.input,*/}
-              {/*  }}*/}
-              {/*/>*/}
+    <ThemeProvider theme={theme}>
+      <form className={style.emailForm} onSubmit={formik.handleSubmit}>
+        <div className={style.itemsWrap}>
+          <div className={style.items}>
+            <div className={style.feedback}>
+              <span className={feedback === 'succeeded' ? style.success : style.successHide}>
+                Your message has been received, I will contact you soon.
+              </span>
             </div>
-            <div className={style.inputWrapper}>
-              <input
-                autoComplete="off"
-                id={'email'}
-                type="text"
-                placeholder="Email *"
-                {...formik.getFieldProps('email')}
+            <div className={style.itemHalf}>
+              <div className={style.inputWrapper}>
+                <TextField
+                  disabled={isSending}
+                  fullWidth
+                  className={'subvariant-hovered'}
+                  id="name"
+                  name="name"
+                  label={formik.errors.name && formik.touched.name ? formik.errors.name : 'Name *'}
+                  variant="outlined"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  error={formik.touched.name && !!formik.errors.name}
+                />
+              </div>
+              <div className={style.inputWrapper}>
+                <TextField
+                  disabled={isSending}
+                  fullWidth
+                  className={'subvariant-hovered'}
+                  id="email"
+                  name="email"
+                  label={
+                    formik.errors.email && formik.touched.email ? formik.errors.email : 'Email *'
+                  }
+                  variant="outlined"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  error={formik.touched.email && !!formik.errors.email}
+                />
+              </div>
+            </div>
+            <div className={style.item}>
+              <textarea
+                disabled={isSending}
+                id={'message'}
+                placeholder="Message"
+                {...formik.getFieldProps('message')}
               />
             </div>
-          </div>
-          <div className={style.item}>
-            <textarea id={'message'} placeholder="Message" {...formik.getFieldProps('message')} />
-          </div>
-          <div className={style.sendMessage}>
-            <button type={'submit'}>Send message</button>
+            <div className={isSending ? style.disabledSendMessage : style.sendMessage}>
+              <button disabled={isSending} className={style.button} type={'submit'}>
+                Send message
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </ThemeProvider>
   )
 }
 
